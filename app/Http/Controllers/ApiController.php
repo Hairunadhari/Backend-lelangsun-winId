746733374a9c8997da7a3df8ab522c5d2085866c
hairunadhari <hairunadhari@gmail.com>
2023-08-08 15:00:43 +0700

    tambah url di api detail logo

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -1148,6 +1148,7 @@ class ApiController extends Controller
      */
     public function detail_toko($id){
         $data = Toko::find($id);
+        $produk->logo = url('https://backendwin.spero-lab.id/storage/image/' . $produk->logo);
         return response()->json([
             'massage' => 'SUCCESS',
             'data' => $data,
