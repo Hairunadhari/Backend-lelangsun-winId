c2bf122340039e6e7977c66deed834995dd16a57
unknown <hairunadhari@gmail.com>
2024-05-11 01:57:12 +0700

    up

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -23,7 +23,7 @@ class RedirectIfAuthenticated
             if ($guard == 'peserta' && Auth::guard($guard)->check()) {
                 return redirect(RouteServiceProvider::PESERTA_HOME);
             }
-            if (Auth::guard($guard)->check()) {
+            if ($guard == 'web' && Auth::guard($guard)->check()) {
                 return redirect()->route('dashboard');
                 // return redirect('superadmin/dashboard');
             }
