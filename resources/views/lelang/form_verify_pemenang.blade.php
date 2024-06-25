fb4dba3e9b5e5f7bf1000ae9c3159c6521a995b2
unknown <hairunadhari@gmail.com>
2024-06-25 13:37:38 +0700

    update

--- resources/views/lelang/form_verify_pemenang.blade.php
+++ resources/views/lelang/form_verify_pemenang.blade.php
@@ -42,8 +42,7 @@
                 </div>
                 @endif
             </div>
-            @if ($data->status_pembayaran != 'Belum Bayar' && $data->npl == null)
-                
+            @if ($data->status_pembayaran == 'Belum Bayar' || $data->nama_pemilik == 'Bidder Offline')
             <div class="card-footer text-right">
                 <button class="btn btn-primary mr-1" type="submit">Verifikasi</button>
             </div>
