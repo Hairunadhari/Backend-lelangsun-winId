de97ce5ef78e7da5c7517b610b50b3e02c733e7d
unknown <hairunadhari@gmail.com>
2024-06-25 13:40:32 +0700

    update

--- resources/views/lelang/form_verify_pemenang.blade.php
+++ resources/views/lelang/form_verify_pemenang.blade.php
@@ -42,7 +42,11 @@
                 </div>
                 @endif
             </div>
-            @if ($data->status_pembayaran != 'Belum Bayar' || $data->nama_pemilik == 'Bidder Offline')
+            @if ($data->status_pembayaran != 'Belum Bayar')
+            <div class="card-footer text-right">
+                <button class="btn btn-primary mr-1" type="submit">Verifikasi</button>
+            </div>
+            @elseif($data->nama_pemilik == 'Bidder Offline')
             <div class="card-footer text-right">
                 <button class="btn btn-primary mr-1" type="submit">Verifikasi</button>
             </div>
